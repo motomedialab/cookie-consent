87c84d5bfcd6b36a3eb678075c9efc7c9069d3ae
Chris Page <chris@motocom.co.uk>
2023-12-07 14:50:05 +0000

    Made data optional Made package 0.4.0

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,52 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import CookieConsent from './CookieConsent.vue'
 
 type cookieDetails = {
-  name: string
-  expires?: string | null
-  description: string
+    name: string
+    expires?: string | null
+    description: string
 }
 
 type preferenceTypes = {
-  functionality: boolean
-  ad: boolean
-  analytics: boolean
-  personalization: boolean
-  security: boolean
+    functionality: boolean
+    ad: boolean
+    analytics: boolean
+    personalization: boolean
+    security: boolean
 }
 
 type cookieWindow = Window &
-  typeof globalThis & {
+    typeof globalThis & {
     cookieConsent: {
-      hide?: boolean
-      title?: string
-      description?: string
-      cookiePolicy: null | string
-      styling: {
-        mode: ['light' | 'dark']
-        buttonColor: string
-      }
-      settings: {
-        [key in keyof preferenceTypes]: {
-          title?: string
-          essential?: boolean
-          description: string
-          cookies?: cookieDetails[]
+        hide?: boolean
+        title?: string
+        description?: string
+        cookiePolicy: null | string
+        styling: {
+            mode: ['light' | 'dark']
+            buttonColor: string
         }
-      }
+        settings: {
+            [key in keyof preferenceTypes]: {
+                title?: string
+                essential?: boolean
+                description: string
+                cookies?: cookieDetails[]
+            }
+        }
     }
-  }
+}
 
-export type { cookieWindow, preferenceTypes }
+export type {cookieWindow, preferenceTypes}
 
 const data = (window as cookieWindow).cookieConsent
 
-if (!data.hide) {
-  const mountPoint = document.createElement('div')
-  mountPoint.id = 'cookie-consent'
-  document.body.append(mountPoint)
+if (!data?.hide) {
+    const mountPoint = document.createElement('div')
+    mountPoint.id = 'cookie-consent'
+    document.body.append(mountPoint)
 
-  createApp(CookieConsent, {
-    data: data
-  }).mount(mountPoint)
+    createApp(CookieConsent, {
+        data: data
+    }).mount(mountPoint)
 }
