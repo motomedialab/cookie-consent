7823d5b5aa36136abb176d06898dd57da9079921
Charlie Johnson <charlie.johnson@motocom.co.uk>
2024-04-22 16:16:54 +0100

    Returned default position to right

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ const data: cookieConsent = deepMerge(
     },
     styling: {
       buttonColor: '#737373',
-      position: 'left'
+      position: 'right'
     }
   } as cookieConsent,
   (window as cookieWindow).cookieConsent ?? {}
