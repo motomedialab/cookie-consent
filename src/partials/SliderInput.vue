1472527caf050468ee96b20a5a54e96c1469d236
Chris Page <chris@motocom.co.uk>
2024-09-16 15:27:28 +0100

    Replaced padding-0 for p-0 and tagged v1.1.4

--- src/partials/SliderInput.vue
+++ src/partials/SliderInput.vue
@@ -25,7 +25,7 @@ defineEmits(['update:modelValue'])
   @apply relative inline-block w-10 h-6 rounded-full shadow-inner cursor-pointer bg-neutral-400/60 hover:bg-neutral-400/70 transition-colors;
 
   .__slider {
-    @apply absolute padding-0 top-0 left-0 bottom-0 rounded-full w-5 h-5 my-[2px] bg-white shadow-lg;
+    @apply absolute p-0 top-0 left-0 bottom-0 rounded-full w-5 h-5 my-[2px] bg-white shadow-lg;
 
     @apply will-change-transform transition translate-x-[2px];
   }
